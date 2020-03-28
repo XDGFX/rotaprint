f3b78b0d4253fad9cb268924fddfed331b46034b
Callum <callum.morrison@mac.com>
2020-03-28 17:42:30 +0000

    Websocket handler, with gcode stream working!

--- rotaprint.py
+++ rotaprint.py
@@ -46,6 +46,7 @@ def get_args():
 def setup_log():
     # Set log level
     log = logging.getLogger("logger")
+    logging.getLogger("websockets").setLevel(logging.WARNING)
 
     if args.debug:
         logging.basicConfig(level=logging.DEBUG,
@@ -178,7 +179,7 @@ class websocket:
         logging.info("Initialising websocket instance")
         # Create thread to run websocket.listen as a daemon (in background)
         listenThread = threading.Thread(target=self.listen)
-        listenThread.daemon = True
+        listenThread.daemon = False
         listenThread.start()
         logging.info("Websocket initialised")
 
@@ -189,10 +190,12 @@ class websocket:
 
         async def listener(websocket, path):
             hold = False
+            buffer = list()
             while True:
                 # Listen for new messages
                 message = await websocket.recv()
                 log.debug(f'WSKT > {message}')
+                await websocket.send("done")
 
                 if message == "BFH":
                     # Buffer hold condition
@@ -201,26 +204,97 @@ class websocket:
                 elif message == "BFR":
                     # Buffer release condition
                     hold = False
+                elif message == "GCD":
+                    # Load next message and send
+                    gcode = await websocket.recv()
+                    gcode = [line.strip()
+                             for line in gcode.decode().split("\n")]
+                    print(gcode)
+                    continue
 
                 if hold:
                     buffer.append(message)
                 else:
                     if buffer:
                         # Buffer has data
-                        threading.Thread(target=self.handler, args=(buffer, ))
+                        self.handler(buffer)
+
+                        # threading.Thread(target=self.handler, args=(buffer, ))
+                        # Clear buffer
+                        buffer = list()
                     else:
                         # Normal message (convert to list)
-                        threading.Thread(target=self.handler,
-                                         args=([message], ))
+                        self.handler([message])
+
+                        # threading.Thread(target=self.handler, args=([message], ))
 
         asyncio.set_event_loop(asyncio.new_event_loop())
-        server = websockets.serve(listener, 'localhost', 8765)
+        server = websockets.serve(listener, 'localhost', 8765, max_size=None)
+        log.warning("No file size limit set on websocket connection")
+        log.warning("This may cause issues when trying to upload large files")
 
         asyncio.get_event_loop().run_until_complete(server)
         asyncio.get_event_loop().run_forever()
 
     def handler(self, message):
-        pass
+        def emergency_stop(self):
+            print("EST")
+            pass
+
+        def buffer_hold(self):
+            pass
+
+        def buffer_release(self):
+            pass
+
+        def set_length(self, data):
+            pass
+
+        def set_batch(self, data):
+            pass
+
+        def set_radius(self, data):
+            pass
+
+        def set_port(self, data):
+            pass
+
+        def toggle_check_mode(self, data):
+            pass
+
+        def set_grbl_setting(self, data):
+            pass
+
+        def send_manual(self, data):
+            pass
+
+        switcher = {
+            "EST": emergency_stop,
+            "BFH": buffer_hold,
+            "BFR": buffer_release,
+            "LEN": set_length,
+            "BAT": set_batch,
+            "RAD": set_radius,
+            "PRT": set_port,
+            "CHK": toggle_check_mode,
+            "SET": set_grbl_setting,
+            "GRB": send_manual,
+        }
+
+        for item in message:
+            # Separate command and data
+            item = item.split("~")
+
+            # Execute command
+            if len(item) == 2:
+                # Message has data
+                switcher[item[0]](self, item[1])
+            elif item[0] == "":
+                # Blank message
+                pass
+            elif len(item) == 1:
+                # Message has no data
+                switcher[item[0]](self)
 
 
 class grbl:
@@ -562,7 +636,7 @@ report_interval = 1.0  # In seconds, if enable_status_reports is True
 
 
 # Load selected gcode file into list
-logging.debug("Loading supplied arguments")
+logging.debug("Loading supplied gcode")
 gcode = gcode_load(args.gcode)
 br()
 # Convert commands where printing to G1 to indicate printing
--- tests/hello.py
+++ tests/hello.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
+
 from flask import Flask, send_from_directory
+from flask_cors import CORS
+import logging
+
+
 app = Flask(__name__, static_url_path='')
+CORS(app)
+
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 
 @app.route('/')
 def hello_world():
-    return app.send_static_file('db_test.html')
+    return app.send_static_file('websockets_test.html')
 
 
 if __name__ == '__main__':
