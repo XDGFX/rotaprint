18ad49f95889fd28ca5e87c99d9b02bc69173be4
hv236 <hv236@bath.ac.uk>
2020-04-29 20:23:00 +0200

    Align.py cleaned and commented - round 1 done

--- vision/align.py
+++ vision/align.py
@@ -12,6 +12,10 @@ camera_port = db.settings["video_device"] #device index  to specify which camera
 
 OUTPUTS:
 offset angle value
+
+TERMS USED:
+"score" is refering to the Similarity score obtained after computing OpenCV "structural_similarity" function.
+"angle_step" ----
 """
 
 # import the necessary packages
@@ -27,52 +31,63 @@ qc = db.settings["qc_images"]  #number of comparison images for quality check sp
 camera_port = db.settings["video_device"] #device index  to specify which camera to use
 
 
-def take_picture(camera_port):
-    #Take a picture with camera
+def take_picture(camera_port): ################variable???
+    #Open webcam wanted
+    #Take a picture 
     #Return the picture data
-    
-    cap = cv2.VideoCapture(camera) #open the webcam of choice (0 usually is the default camera)
+    #Release the video capture object
+     
+    log.debug("Activating the desired camera..." ) 
+
+    cap = cv2.VideoCapture(camera) #activate the webcam of choice (0 usually is the default camera)
     
     #Check if camera opened successfully
     if (cap.isOpened()== False):
         log.error("Error opening video stream or file"))
      
-    ret, picture_data = cap.read() # return a single frame in variable `picture_data` 
+    log.info("Camera successfully activated")
+
+    _ , picture_data = cap.read() # return a single frame in variable `picture_data` 
     #ret is a boolean variable that returns true if the frame is available.
     #picture_data is an image array vector captured based on the default frames per second defined explicitly or implicitly
        
     # Default resolutions of the frame are obtained.The default resolutions are system dependent.
     # We convert the resolutions from float to integer.
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
+    picture_width = int(cap.get(3))
+    picture_height = int(cap.get(4))
 
     # When everything done, release the video capture object
     cap.release()
 
-
     return picture_data 
 
-def rotate_and_picture(r):  # r is the number of picture
+def rotate_and_picture(pictures_total):
+    # Rotate the part at desired angle steps
+    # Take a picture at each rotation
+    # return pictures data in a list 
+    log.warning("Taking more than 360 pictures may slow down the image processing.")
+    log.info("Rotating and taking a picture at each rotation until {pictures_total} pictures taken.")
+    
+    if picture_total = 0
+        log.error("The number of pictures specified must be above zero!")
+        break##########################################################################################£!!!!!
 
-    # Rotate the part in order to return ynumber for picture, taken every rotation
-    # return an array of
-    pictures_list = np.empty((0,3), int)
-    angle_step = 360/r  # reference
+    #pictures_list = np.empty((0,3), int)
+    pictures_list = []
+    angle_step = 360/pictures_total
 
-    # if y=8: x=[0,45,90,135,180,225,270,315] - will need to return to 0/360 position
-    for x in range(0, r):
-        angle = 360/r*x
+    for x in range(pictures_total):
+        angle = angle_step*x
         g.send([angle], True) # where % is the angle in degrees you want the part to go to
-        picture = take_picture(picture)
-        B, G, R = cv2.split(picture)
+        picture_data = take_picture (camera_port) ###########################################################!!!!!!!
 
-        # record data in array with each R,G,B values associated to rotation (3 column: R,G,B) 0,1,2
-        pictures_list = np.append(pictures_list, np.array([[R,G,B]]), axis=0)
+        # record data in a list
+        pictures_list.append(picture_data) 
 
-
-
+    #go back to initial position
     g.send([angle_step], True) # where % is the angle in degrees you want the part to go to
 
+    log.info("The {pictures_total} number of picture were sucessfully taken ")
     return angle_step, pictures_list
 
 
@@ -97,78 +112,68 @@ g.change_batch(0, True) # Move component 0 under scanner
 
 # engineer manually align the part to desired starting position in reference to laser, using controls in interface.
 
-# Record the y number of images of reference and store image data
-"""angle_step_y, ref_image = rotate_and_picture(r)  
+# Record the "r" number of images of reference and store image data
+angle_step_r, ref_image = rotate_and_picture(r)
 
 
 """Alignement of batch"""
+#take c number of comparison images
+#these should be compared against the reference images as set of "r"
+#################################Explain here a bit better######################################
 
-# import dummy pictures for testing code
-r = 4  # Reference
-ref_images_upload = ["vision/img1.jpg", "vision/img4.jpg", "vision/img7.jpg", "vision/img10.jpg"]
-angle_step_r = 360/r
-
-ref_images = test_split(ref_images_upload)
-
-# test5
-c = 14  # Comparison
-com_images_upload = ["vision/img1.jpg", "vision/img2.jpg", "vision/img3.jpg", "vision/img4.jpg", "vision/img5.jpg", "vision/img6.jpg", "vision/img7.jpg", "vision/img8.jpg", "vision/img9.jpg", "vision/img10.jpg", "vision/img11.jpg", "vision/img12.jpg", "vision/img13.jpg", "vision/img14.jpg"]
-angle_step_c = 360/c
+# next part of batch is moved linearly to sensing position
+g.change_batch(1, True) # Move component 0 under scanner
 
 
-com_images = test_split(com_images_upload)
+# record "c" number of comparison images (to be compared against reference image as set of "r")
+angle_step_c, com_images = rotate_and_picture(c)
 
+#Initialisation of lists
+temp_score = [] #temporary list to use within loop to collect SSIM scores of a comparison for each colour 
+score_set = [] # list the scores of each comparison within a set
+scores_per_sets = [] # Main list: list of the averages scores of each set 
 
-# next part is moved linearly to sensing position
-
-# record w number of inspection image to be compared with reference images
-
-# w to be chosen for accuracy
-"""angle_step_w, com_images = rotate_and_picture(w)"""
-
-# initialise the lists
-scores_per_sets = [] #
-temp_score = []
-score_set = []
-
-# Step size for W wrt Y
+#compute the ideal step size to compare 
 ideal_step = c / r
 
-# Increment start comparison image
+
 for start in range(c):
-
-    # Determine y number of comparison images to test against the reference images
-    # round j to nearest integer
+    # Increment start comparison image
+    
     for j, i  in zip(np.arange(start, start + c, ideal_step,int), range(r)): 
+    # Compare the comparison image against its supposedly correct the reference images (in terms of angle)
+    # Check the score of similarity between those two images
+    # round j to nearest integer
 
-        # If image index is greater than the number of images, 'wrap around' to the start
+        # If image index is greater than the number of images, 'wrap around' to the start - keeping the given angle step
         if j >= c:
             j = j - c
                 
         for colour in range(3):
-            #loop over R,G,B pixels
+            #loop over B,G,R pixels - in that order
             (score, _) = structural_similarity(ref_images[i][:, :, colour], com_images[j][:, :, colour], full=True)
 
             temp_score.append(score)
 
-        # average the R,G,B scores of that comparison and assign to one list 
-        temp_score_avg= statistics.mean (temp_score) 
-        score_set.append(temp_score_avg)
-        temp_score.clear() 
-    score_set_avg = statistics.mean (score_set)
-    scores_per_sets.append (score_set_avg)
-    score_set.clear()
+        
+        temp_score_avg= statistics.mean (temp_score)  #average the three colours scores for the given comparison
+        score_set.append(temp_score_avg) #store that averaged score in score_set list
+        temp_score.clear() #clear list for next comparison 
+
+       
+    score_set_avg = statistics.mean (score_set) #after a set, average all scores of set
+    scores_per_sets.append (score_set_avg) #store that average set score in the main list
+    score_set.clear() #clear list for next set
 
        
 
-# find the highest score of all y sets
+# find the highest score of all "c" sets
 # return the index
 maxi = scores_per_sets.index(max(scores_per_sets))
 
-print("Most similar set:" + com_images[maxi])
 
-# the index indicate the image hence the rotation
-##g.send([angle_step_w*maxi], True) # where % is the angle in degrees you want the part to go to
+# the index indicates the offset angle to align part to desired priting position
+g.send([angle_step_w*maxi], True) # where % is the angle in degrees you want the part to go to
 
 
 
